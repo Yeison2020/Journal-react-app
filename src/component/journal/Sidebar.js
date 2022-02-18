c90581b3c62a4ade4305640fcdd2f2ca712f0aa4
Yeison <enmanuelquiterios@gmail.com>
2022-02-18 10:47:54 -0500

    Created sidebar styles and new Entry button

--- src/component/journal/Sidebar.js
+++ src/component/journal/Sidebar.js
@@ -4,9 +4,15 @@ const Sidebar = () => {
   return (
     <aside className="journal__sidebar">
       <div className="journa__sidebar-navbar">
-        <h3>
-          <span>Yeison Casado</span>
+        <h3 className="mt-5">
+          <i className="far fa-moon"></i>
+          <span> Yeison Casado</span>
         </h3>
+        <button className="btn">Logout</button>
+      </div>
+      <div className="journal__new-entry">
+        <i className="fa fa-calendar-plus fa-5x"></i>
+        <p className="mt-5">New Entry</p>
       </div>
     </aside>
   );
