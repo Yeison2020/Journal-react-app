a462a2bf0f73751704ca7f6648d6323fdf812476
Yeison <enmanuelquiterios@gmail.com>
2022-02-18 11:09:42 -0500

    Adding single entry

---
+++ src/component/journal/JournalEntry.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JournalEntry = ({ value }) => {
+  return <div>{value}</div>;
+};
+
+export default JournalEntry;
--- src/component/journal/Sidebar.js
+++ src/component/journal/Sidebar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import JournalEntries from "./JournalEntries ";
 
 const Sidebar = () => {
   return (
     <aside className="journal__sidebar">
-      <div className="journa__sidebar-navbar">
+      <div className="journal__sidebar-navbar">
         <h3 className="mt-5">
           <i className="far fa-moon"></i>
           <span> Yeison Casado</span>
@@ -14,6 +15,7 @@ const Sidebar = () => {
         <i className="fa fa-calendar-plus fa-5x"></i>
         <p className="mt-5">New Entry</p>
       </div>
+      <JournalEntries />
     </aside>
   );
 };
