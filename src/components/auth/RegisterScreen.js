d1588a74efd8a3f439a02b9f8dab3286280ef684
Yeison <enmanuelquiterios@gmail.com>
2022-02-20 11:17:12 -0500

    Added validation to my UI from the redux state

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "../../hooks/useForm";
 import validator from "validator";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setError, removeError } from "../../actions/ui";
 
 const RegisterScreen = () => {
   const dispatch = useDispatch();
+  const { msgError } = useSelector((state) => state.ui);
+
   const [values, handleInputChange, reset] = useForm({
     name: "",
     email: "",
@@ -20,7 +22,7 @@ const RegisterScreen = () => {
       dispatch(setError("Name is requiered"));
       return false;
     } else if (!validator.isEmail(email)) {
-      dispatch(setError("Email is not corrent"));
+      dispatch(setError("Email is not valid"));
 
       return false;
     } else if (password !== password2 || password.length < 5) {
@@ -45,7 +47,7 @@ const RegisterScreen = () => {
     <>
       <h3 className="auth__title">Register</h3>
       <form onSubmit={handleRegister}>
-        <div className="auth__alert-error">Hello World</div>
+        {msgError && <div className="auth__alert-error">{msgError}</div>}
         <input
           className="auth__input"
           type="text"
