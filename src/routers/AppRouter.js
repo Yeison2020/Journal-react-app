86fe9f81d2b7482b92c11bbe0a470fb30cc8177c
Yeison <enmanuelquiterios@gmail.com>
2022-02-26 12:02:09 -0500

    Added two useStates to check If user is logged in and another to display while we wait for the useEffect to check for my user

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,13 +13,28 @@ import { login } from "../actions/auth";
 
 const AppRouter = () => {
   const dispatch = useDispatch();
+  const [checking, setChecking] = useState(true);
+
+  // Here My isLoggedIn will controll who see my current routes
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Here is how I'm adding my current users to my redux states using this useEffect Notes: I need to fixed when user try to log in and Gets an errors in the UI login like password or incorrect email. There is a bug there when the btn gets the loading state.
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user?.uid) {
+        setIsLoggedIn(true);
         dispatch(login(user.uid, user.displayName));
+      } else {
+        setIsLoggedIn(false);
       }
+
+      // Importan how I setted this to wait and load another component until I get the answer from my UseEffect
+      setChecking(false);
     });
-  }, []);
+  }, [dispatch]);
+  if (checking) {
+    return <h1>Please Wait Checking user</h1>;
+  }
 
   return (
     <>
